e19be7ceca862be6be161c51a2dd21c4bc71b81e
Roman Marchenko <r.marchenko@samsung.com>
2014-10-02 16:23:36 +0300

    DRI3: Add checking of size and pitch modify libtbm. Move code(export and import functions) from driver to libtbm. Code formatting.
    Change-Id: Ie420a25ef16e07e1e8dd9b2de13e8fbc7eb66cdb
    Signed-off-by: Roman Marchenko <r.marchenko@samsung.com>

--- src/accel/sec_dri3.c
+++ src/accel/sec_dri3.c
@@ -52,129 +52,126 @@
 #include "sec_accel.h"
 
 // -------------------------- Private functions--------------------------------
-/* TODO : _boCreateFromFd and  int _tbm_bo_import_fd mast be moved to _tbm_bo_export_fd */
-
-tbm_bo _tbm_bo_import_fd(int drm_fd, tbm_bufmgr bufmgr, int prime_fd, int size)
-{
-	//getting handle from fd
-	unsigned int gem = 0;
-	if (drmPrimeFDToHandle(drm_fd, prime_fd, &gem))
-	{
-		return NULL;
-	}
-
-	//getting name from handle(gem)
-    struct drm_gem_flink arg_flink = {0,};
-    arg_flink.handle = gem;
-    if (drmIoctl(drm_fd, DRM_IOCTL_GEM_FLINK, &arg_flink))
-    {
-    	return NULL;
-    }
-
-    //creating tbm_bo from name
-    return tbm_bo_import(bufmgr, arg_flink.name);
-}
-
-int _tbm_bo_export_fd(tbm_bo bo, int *fd)
-{
-	tbm_bo_handle handle = tbm_bo_get_handle(bo, TBM_DEVICE_MM);
-	*fd = handle.s32;
-	return 0;
-}
-
 
 // -------------------------- Callback functions--------------------------------
 static int
-SECDRI3Open(ScreenPtr screen, RRProviderPtr provider, int *fdp) {
-	ScrnInfoPtr pScrn = xf86ScreenToScrn(screen);
-	SECPtr pExynos = SECPTR(pScrn);
-	drm_magic_t magic;
-	int fd;
+SECDRI3Open(ScreenPtr screen, RRProviderPtr provider, int *fdp)
+{
+    ScrnInfoPtr pScrn = xf86ScreenToScrn(screen);
+    SECPtr pExynos = SECPTR(pScrn);
+    drm_magic_t magic;
+    int fd;
 
-	/* Open the device for the client */
-	fd = open(pExynos->drm_device_name, O_RDWR | O_CLOEXEC);
-	if (fd == -1 && errno == EINVAL) {
-		fd = open(pExynos->drm_device_name, O_RDWR);
-		if (fd != -1)
-			fcntl(fd, F_SETFD, fcntl(fd, F_GETFD) | FD_CLOEXEC);
-	}
+    /* Open the device for the client */
+    fd = open(pExynos->drm_device_name, O_RDWR | O_CLOEXEC);
+    if (fd == -1 && errno == EINVAL)
+    {
+        fd = open(pExynos->drm_device_name, O_RDWR);
+        if (fd != -1)
+            fcntl(fd, F_SETFD, fcntl(fd, F_GETFD) | FD_CLOEXEC);
+    }
 
-	if (fd < 0)
-		return BadAlloc;
+    if (fd < 0)
+        return BadAlloc;
 
-	/* Go through the auth dance locally */
-	if (drmGetMagic(fd, &magic) < 0) {
-		close(fd);
-		return BadMatch;
-	}
+    /* Go through the auth dance locally */
+    if (drmGetMagic(fd, &magic) < 0)
+    {
+        close(fd);
+        return BadMatch;
+    }
 
-	/* And we're done */
-	*fdp = fd;
-	return Success;
+    /* And we're done */
+    *fdp = fd;
+    return Success;
 }
 
-static PixmapPtr SECDRI3PixmapFromFd (ScreenPtr pScreen,
-                                      int fd,
-                                      CARD16 width,
-                                      CARD16 height,
-                                      CARD16 stride,
-                                      CARD8 depth,
-                                      CARD8 bpp)
+int
+_check_bpp(CARD8 bpp)
 {
-	ScrnInfoPtr pScrn = xf86ScreenToScrn(pScreen);
-	SECPtr pSec = SECPTR(pScrn);
+    switch (bpp)
+    {
+    case 8:
+    case 16:
+    case 32:
+        return 1;
+    default:
+        return 0;
+    }
+}
 
+static PixmapPtr 
+SECDRI3PixmapFromFd(ScreenPtr pScreen,
+                    int fd,
+                    CARD16 width,
+                    CARD16 height,
+                    CARD16 stride,
+                    CARD8 depth,
+                    CARD8 bpp)
+{
+    ScrnInfoPtr pScrn = xf86ScreenToScrn(pScreen);
+    SECPtr pSec = SECPTR(pScrn);
 
-	PixmapPtr pPixmap = NULL;
+    PixmapPtr pPixmap = NULL;
 
-	if (depth == 1)
-		return NULL;
+    XDBG_RETURN_VAL_IF_FAIL((width <= INT16_MAX && height <= INT16_MAX), NULL);
 
-    //create bo
-    tbm_bo tbo = _tbm_bo_import_fd(pSec->drm_fd, pSec->tbm_bufmgr,
-    								fd, (uint32_t) height * stride);
+    XDBG_RETURN_VAL_IF_FAIL(((uint32_t )width * bpp <= (uint32_t )stride * 8), NULL);
 
-    if (tbo == NULL)
-    	goto no_bo;
+    XDBG_RETURN_VAL_IF_FAIL((depth > 8), NULL);
 
-    pPixmap = (*pScreen->CreatePixmap) (pScreen, 0, 0, depth,
-    									CREATE_PIXMAP_USAGE_SUB_FB);
+    XDBG_RETURN_VAL_IF_FAIL(_check_bpp(bpp), NULL);
 
-    if (!pPixmap)
-    	goto no_pixmap;
+    tbm_bo tbo = tbm_bo_import_fd(pSec->tbm_bufmgr, fd);
 
-	tbm_bo_ref(tbo);
+    XDBG_RETURN_VAL_IF_FAIL(tbo != NULL, NULL);
+
+    uint32_t real_size = tbm_bo_size(tbo);
+    uint32_t target_size = (uint32_t) height * stride;
+
+    if (real_size < target_size)
+    {
+        XDBG_WARNING(MDRI3, "the real size of bo(%p) less then target: %d, %d\n", tbo, real_size, target_size);
+        goto free_bo;
+    }
+
+    pPixmap = (*pScreen->CreatePixmap)(pScreen, 0, 0, depth,
+    CREATE_PIXMAP_USAGE_SUB_FB);
+
+    XDBG_GOTO_IF_FAIL(pPixmap != NULL, free_bo);
+
     if (!pScreen->ModifyPixmapHeader(pPixmap, width, height, 0, 0, stride, tbo))
-    	goto no_modify;
+        goto free_pix;
 
     return pPixmap;
 
-no_modify:
-	(*pScreen->DestroyPixmap)(pPixmap);
-no_pixmap:
-	tbm_bo_unref(tbo);
-no_bo:
+free_pix:
+    (*pScreen->DestroyPixmap)(pPixmap);
+free_bo:
+    tbm_bo_unref(tbo);
+
     return NULL;
 }
 
-static int SECDRI3FdFromPixmap (ScreenPtr pScreen,
-                                PixmapPtr pPixmap,
-                                CARD16 *stride,
-                                CARD32 *size)
+static int
+SECDRI3FdFromPixmap(ScreenPtr pScreen,
+                    PixmapPtr pPixmap,
+                    CARD16 *stride,
+                    CARD32 *size)
 {
-	SECPixmapPriv * priv = NULL;
-	int fd;
-	int ret;
+    SECPixmapPriv * priv = NULL;
+    int fd;
+    int ret;
 
-	priv = exaGetPixmapDriverPrivate(pPixmap);
-	if (!priv)
-		return -1;
-	ret = _tbm_bo_export_fd(priv->bo, &fd);
-	if (ret < 0)
-		return -1;
-	*stride = priv->stride;
-	*size = tbm_bo_size(priv->bo);
-	return fd;
+    priv = exaGetPixmapDriverPrivate(pPixmap);
+    if (!priv)
+        return -1;
+    ret = tbm_bo_export_fd(priv->bo);
+    if (ret <= 0)
+        return -1;
+    *stride = priv->stride;
+    *size = tbm_bo_size(priv->bo);
+    return fd;
 }
 
 static dri3_screen_info_rec sec_dri3_screen_info = {
