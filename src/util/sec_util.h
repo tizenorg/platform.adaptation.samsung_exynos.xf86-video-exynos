9cdd94561438ba5d030f1ba9dadc008db97493f4
Junkyeong Kim <jk0430.kim@samsung.com>
2014-12-24 14:58:19 +0900

    init xv clone adaptor
    Change-Id: Ia546183cb87d99eb1abd8e37256ae1ca7c1f532f
    Signed-off-by: Junkyeong Kim <jk0430.kim@samsung.com>

--- src/util/sec_util.h
+++ src/util/sec_util.h
@@ -70,6 +70,7 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #define MEXAH   XDBG_M('E','X','A','H')
 #define MG2D    XDBG_M('G','2','D',0)
 #define MDOUT   XDBG_M('D','O','T',0)
+#define MCLON   XDBG_M('C','L','O','N')
 
 #define _XID(win)   ((unsigned int)(((WindowPtr)win)->drawable.id))
 
